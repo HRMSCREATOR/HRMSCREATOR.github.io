166c3fb2c5f0cc1160f5fc3bd204198fd0c5234f
HRMSCREATOR <hotelroommanagementsystem@gmail.com>
2025-02-19 22:47:34 +0800

    jsLoops.js

--- jsLoops.js
+++ jsLoops.js
@@ -1,1 +1,26 @@
+//It handle a clicking function that show in the html view
+document.getElementById('loopme').addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    const input = parseFloat(document.getElementById('numbers').value);
+    let results;
+
+    function checkEvenOrOdd(n) {
+        if (n % 2 === 0) {
+            results = "Even";
+            
+        } else {
+            results = "Odd";
+        }
+       document.getElementById("result").innerText = results;
+       return alert(`The type of this, number: ${input} is result to ${results}`)
+    }
+
+    checkEvenOrOdd(input)
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(`Counting: ${i},  I love programming😍.`);
+    }
+
+
+}); 
